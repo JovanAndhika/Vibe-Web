599c58c96a93d3b122094b7fb53108a96f5623bf
JackGam31 <c14220039@john.petra.ac.id>
2023-11-13 21:54:55 +0700

    Reset to initial commit

--- routes/web.php
+++ routes/web.php
@@ -16,35 +16,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::get('/hello', function () {
-    return 'Hello World';
-});
-
-Route::get('/about', function () {
-    return 'Hiiii';
-});
-
-
-Route::get('/home', function () {
-    return 'gatel';
-});
-
-
-Route::get('/home', function () {
-    return 'gatel';
-});
-
-Route::get('/about', function () {
-    return 'Hellllooooo';
-});
-
-
-Route::get('/about', function () {
-    return 'Hellllooooo';
-});
-
-
-Route::get('/about', function () {
-    return 'Hellllooooo';
-});
