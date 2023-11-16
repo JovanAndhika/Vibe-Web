42a141ccc4bdbdf183587d4c066e7676135b001a
Jovan Andhika <c14220013@john.petra.ac.id>
2023-11-16 23:39:42 +0700

    change music

--- database/migrations/2023_11_13_151752_create_musics_table.php
+++ database/migrations/2023_11_13_151752_create_musics_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('musics', function (Blueprint $table) {
+        Schema::create('music', function (Blueprint $table) {
             $table->id();
             $table->string('title');
             $table->string('artist');
@@ -27,6 +27,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('musics');
+        Schema::dropIfExists('music');
     }
 };
