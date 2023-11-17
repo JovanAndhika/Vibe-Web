acb9559ed9e5d688840d33084facfae5d9f28385
royJuanAndika <118645021+royJuanAndika@users.noreply.github.com>
2023-11-17 20:46:00 +0700

    rubah icon navleft

--- resources/views/partials/navleft.blade.php
+++ resources/views/partials/navleft.blade.php
@@ -2,7 +2,7 @@
 class="col-3 p-0 m-0 fontMonsseratExtraBold d-flex align-items-center justify-content-center resizable">
 <ul class="nav flex-column text-white hoverable">
     <li class="nav-item d-inline-flex align-items-center my-3">
-        <i class="bi bi-play-circle-fill text-white @if($active == 'nowPlaying') active @endif"></i>
+        <i class="bi bi-play-circle text-white @if($active == 'nowPlaying') active @endif"></i>
         <a class="nav-link text-white @if($active == 'nowPlaying') active @endif" href="/nowPlaying#jumphere">Now Playing</a>
     </li>
 
@@ -17,8 +17,8 @@ class="col-3 p-0 m-0 fontMonsseratExtraBold d-flex align-items-center justify-co
     </li>
 
     <li class="nav-item d-inline-flex align-items-center my-3">
-        <i class="bi bi-search text-white @if($active == 'discoverPlaylist') active @endif"></i>
-        <a class="nav-link text-white @if($active == 'discoverPlaylist') active @endif" href="/discoverPlaylist#mainpage#jumphere">Discover Playlist</a>
+        <i class="bi bi-compass text-white @if($active == 'discoverPlaylist') active @endif"></i>
+        <a class="nav-link text-white @if($active == 'discoverPlaylist') active @endif" href="/discoverPlaylist#jumphere">Discover Playlist</a>
     </li>
 
     <li class="nav-item d-inline-flex align-items-center my-3">
@@ -26,7 +26,7 @@ class="col-3 p-0 m-0 fontMonsseratExtraBold d-flex align-items-center justify-co
         <a class="nav-link text-white @if($active == 'history') active @endif" href="/history#jumphere">History</a>
     </li>
     <li class="nav-item d-inline-flex align-items-center my-3">
-        <i class="bi bi-collection-play-fill text-white @if($active == 'library') active @endif"></i>
+        <i class="bi bi-collection-play text-white @if($active == 'library') active @endif"></i>
         <a class="nav-link text-white @if($active == 'library') active @endif" href="/library#jumphere">Library</a>
     </li>
 
