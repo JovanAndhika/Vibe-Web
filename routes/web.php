495fe35a55825d2bf100b60c44c89d41cfc0b927
JackGam31 <c14220039@john.petra.ac.id>
2023-11-17 23:33:26 +0700

    merapikan dan menambahkan middleware

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\RegisterController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AdminController;
 use App\Http\Controllers\MusicController;
@@ -19,14 +21,31 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/testing', [MusicController::class, 'index']);
-
-Route::get('/hello', function () {
-    return 'Hello World';
+// Before Login
+Route::group(['middleware' => 'guest'], function(){
+    Route::get('/register', [RegisterController::class, 'index'])->name('register');
+    Route::post('/register', [RegisterController::class, 'store'])->name('register.store');
+    Route::get('/login', [LoginController::class, 'index'])->name('login');
+    Route::post('/login', [LoginController::class, 'authenticate'])->name('login.store');
 });
 
-Route::get('/admin', [MusicController::class, 'index'])->name('admin.index');
-Route::get('/admin/addsong', [MusicController::class, 'add_song'])->name('admin.add');
-Route::post('/admin/store', [MusicController::class, 'store_song'])->name('admin.store');
-Route::get('/admin/editsong/{music}', [MusicController::class, 'edit_song'])->name('admin.edit');
-Route::put('/admin/update/{music}', [MusicController::class, 'update_song'])->name('admin.update');
+// User
+Route::group(['middleware' => 'auth'], function(){
+    // user
+    Route::group(['middleware' => 'user', 'prefix' => 'user', 'as' => 'user.'], function () {
+        // TODO: buat user controller
+        Route::get('/', function () { return view('user.temp_index'); })->name('index');
+    });
+
+    // admin
+    Route::group(['middleware' => 'admin', 'as' => 'admin.'], function () {
+        Route::get('/admin', [MusicController::class, 'index'])->name('index');
+        Route::get('/admin/addsong', [MusicController::class, 'add_song'])->name('add');
+        Route::post('/admin/store', [MusicController::class, 'store_song'])->name('store');
+        Route::get('/admin/editsong/{music}', [MusicController::class, 'edit_song'])->name('edit');
+        Route::put('/admin/update/{music}', [MusicController::class, 'update_song'])->name('update');
+    });
+
+    // logout
+    Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
+});
