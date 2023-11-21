d6d76dcdee123921507563ebe1ac5a009e276207
Daniel12479 <danielchristopherjuwono@gmail.com>
2023-11-21 14:25:36 +0700

    Tambahan playlist dan nowplaying

--- resources/views/user/nowPlaying.blade.php
+++ resources/views/user/nowPlaying.blade.php
@@ -27,10 +27,8 @@
                 <div class="row">
                     <div class="col d-flex justify-content-center my-3" style="font-size: 30px;">
                         {{-- TODO: rapikan/hilangkan tampilan play mp3 --}}
-                        <audio controls><source src="{{ asset('storage/' . $music->file_path) }}" type="audio/mpeg"></audio>
-                        <i class="bi bi-play-fill mx-3"></i>
-                        <i class="bi bi-pause-fill mx-3"></i>
                         <i class="bi bi-bookmark-fill mx-3"></i>
+                        <audio controls><source src="{{ asset('storage/' . $music->file_path) }}" type="audio/mpeg"></audio>
                         <i class="bi bi-heart-fill mx-3"></i>
                     </div>
                 </div>
@@ -41,6 +39,47 @@
                     <h1 class="fontMonsseratSemiBold" style="font-size: 20px;">No Music Currently Playing</h1>
                 </div>
             @endif
+        </div>
+        <div class="container-fluid text-left mb-5" id= "2">
+            
+            <table class="table table-striped table-hover table-dark fontMonsseratRegular">
+                <thead>
+                  <tr>
+                    <th scope="col">Song</th>
+                    <th scope="col">Artist</th>
+                    <th scope="col">Time</th>
+                    <th scope="col"></th>
+
+
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <th>Yuk Sri</th>
+                    <th>Bayu Skak</th>
+                    <th>18:01:05</th>
+                    <th><a href="/nowPlaying"><i class="bi bi-play-fill text-white"></i></a></th>
+
+                  
+                  </tr>
+                  <tr>
+                    <th>Sasageyo</th>
+                    <th>Hiroyuki Sawano</th>
+                    <th>12:51:18</th>
+                    <th><a href="/nowPlaying"><i class="bi bi-play-fill text-white"></i></a></th>
+
+     
+                  </tr>
+                  <tr>
+                    <th>Happy Ya Ya</th>
+                    <th>Guru Sekolah Minggu</th>
+                    <th>07:22:14</th>
+                    <th><a href="/nowPlaying"><i class="bi bi-play-fill text-white"></i></a></th>
+
+                    
+                  </tr>
+                </tbody>
+              </table>
         </div>
     </div>
     </div>
--- resources/views/user/partials/modalDiscoverPlaylist.blade.php
+++ resources/views/user/partials/modalDiscoverPlaylist.blade.php
@@ -8,32 +8,43 @@
             </div>
             <div class="modal-body fontMonsseratRegular bg-dark">
                 <h1 class="fontMonsseratSemiBold" style="font-size: 20px;">Playlist Name</h1>
-                <table class="table table-striped table-hover table-dark mb-5">
-                    <thead>
-                        <tr>
-                            <th class="fontMonsseratSemiBold" scope="col">Song</th>
-                            <th class="fontMonsseratSemiBold" scope="col">Artist</th>
-                            <th class="fontMonsseratSemiBold" scope="col"></th>
-                        </tr>
-                    </thead>
-                    <tbody class="text-left">
-                        <tr>
-                            <th>Ghost<!-- @JackGame31 @JovanAndhika ini title --></th>
-                            <th>Justin Bieber <!-- @JackGame31 @JovanAndhika ini artist --></th>
-                            <th><a href=""><i class="bi bi-plus-lg text-white"></i></a></th>
-                        </tr>
-                        <tr>
+                <div class="container-fluid text-left mb-5" id= "2">
+            
+                    <table class="table table-striped table-hover table-dark fontMonsseratRegular">
+                        <thead>
+                          <tr>
+                            <th scope="col">Song</th>
+                            <th scope="col">Artist</th>
+                            <th scope="col"></th>
+        
+        
+                          </tr>
+                        </thead>
+                        <tbody>
+                          <tr>
+                            <th>Yuk Sri</th>
+                            <th>Bayu Skak</th>
+                            <th><a href="/nowPlaying"><i class="bi bi-play-fill text-white"></i></a></th>
+        
+                          
+                          </tr>
+                          <tr>
                             <th>Sasageyo</th>
                             <th>Hiroyuki Sawano</th>
-                            <th><a href=""><i class="bi bi-plus-lg text-white"></i></a></th>
-                        </tr>
-                        <tr>
+                            <th><a href="/nowPlaying"><i class="bi bi-play-fill text-white"></i></a></th>
+        
+             
+                          </tr>
+                          <tr>
                             <th>Happy Ya Ya</th>
                             <th>Guru Sekolah Minggu</th>
-                            <th><a href=""><i class="bi bi-plus-lg text-white"></i></a></th>
-                        </tr>
-                    </tbody>
-                </table>
+                            <th><a href="/nowPlaying"><i class="bi bi-play-fill text-white"></i></a></th>
+        
+                            
+                          </tr>
+                        </tbody>
+                      </table>
+                </div>
             </div>
             <div class="modal-footer" style="background-color: rgb(238, 181, 0)">
                 <button type="button" class="btn btn-outline-dark fontMonsseratSemiBold"
