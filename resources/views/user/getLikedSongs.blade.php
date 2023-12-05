56ff8905d8390232701a26128118f9871bc525b6
Jovan Andhika <c14220013@john.petra.ac.id>
2023-12-06 02:31:31 +0700

    fix: tombol di like

--- resources/views/user/getLikedSongs.blade.php
+++ resources/views/user/getLikedSongs.blade.php
@@ -5,12 +5,15 @@
 <tr>
     <td>{{ $m['title'] }}</td>
     <td>{{ $m['artist'] }}<!-- @JackGame31 @JovanAndhika ini artist --></td>
-    <td><a href="{{ route('user.nowPlaying') }}?music_id={{ $m['id_music'] }}"><i class="bi bi-play-fill text-white"></i></a>
-        <form action="{{ route( 'user.deletelike', ['music_playlist' => $music] ) }}" method="post">
-            @method('delete')
-            @csrf
-            <button type="submit" class="btn btn-dark" name="addlike" id="addlike"><i class="bi bi-trash-fill text-white"></i></button>
-        </form>
+    <td>
+        <div class="d-flex flex-row">
+            <a href="{{ route('user.nowPlaying') }}?music_id={{ $m['id_music'] }}"><i class="bi bi-play-fill text-white"></i></a>
+            <form action="{{ route( 'user.deletelike', ['music_playlist' => $music] ) }}" method="post">
+                @method('delete')
+                @csrf
+                <button type="submit" class="btn btn-sm btn-dark" name="addlike" id="addlike"><i class="bi bi-trash-fill text-white"></i></button>
+            </form>
+        </div>
     </td>
 </tr>
 @endforeach
