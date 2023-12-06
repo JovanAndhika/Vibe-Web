ef88863b64a5a5f9f4c45019b0c253a6fd2ab7a2
JackGam31 <c14220039@john.petra.ac.id>
2023-12-06 12:47:32 +0700

    Menambah bookmark dan merapikan

--- app/Http/Controllers/User/PlaylistController.php
+++ app/Http/Controllers/User/PlaylistController.php
@@ -3,51 +3,117 @@
 namespace App\Http\Controllers\User;
 
 use App\Models\Music;
+use App\Models\Music_playlist;
 use App\Models\Playlist;
 use Illuminate\Http\Request;
 use App\Models\PlaylistEntities;
 use App\Http\Controllers\Controller;
 
-class PlaylistController extends Controller
-{
-    public function getAllMusics()
-    {
+class PlaylistController extends Controller {
+    public function getAllMusics() {
         // jika ada request search
         $musics = collect([]);
         // cari data music berdasarkan judul
-        $musics = Music::where('title', 'LIKE', "%" . request('search') . "%")
-            ->orWhere('artist', 'LIKE', "%" . request('search') . "%")
+        $musics = Music::where('title', 'LIKE', "%".request('search')."%")
+            ->orWhere('artist', 'LIKE', "%".request('search')."%")
             ->orderBy('release_date', 'desc')
             ->take(10)
             ->get();
         return $musics->toArray();
     }
 
+    public function getAllPlaylist() {
+        $music_id = request('music_id');
+        $user_id = auth()->user()->id;
+
+        // Retrieve all playlists owned by the current user
+        $playlists = Playlist::where('user_id', $user_id)->get();
+
+        // Add the 'is_added' attribute to each playlist
+        foreach($playlists as &$playlist) {
+            // Check if the current song is already in the playlist
+            $is_added = Music_playlist::where('music_id', $music_id)
+                ->where('playlist_id', $playlist->id)
+                ->exists();
+
+            $playlist->is_added = $is_added;
+        }
+
+        // If you want to convert the result to an array, you can use toArray()
+        $playlistsArray = $playlists->toArray();
+
+        // If you want to return the playlists as JSON
+        return response()->json($playlistsArray);
+    }
+
+    public function updateMusic() {
+        $music_id = request('music_id');
+        $playlist_id = request('playlist_id');
+        $is_added = request('is_added');
+
+        if($is_added) {
+            // jika sudah ada, maka hapus
+            Music_playlist::where('music_id', $music_id)
+                ->where('playlist_id', $playlist_id)
+                ->delete();
+            return response()->json(['message' => 'Successfully removed from playlist']);
+        } else {
+            // jika belum ada, maka tambahkan
+            Music_playlist::create([
+                'music_id' => $music_id,
+                'playlist_id' => $playlist_id
+            ]);
+            return response()->json(['message' => 'Successfully added to playlist']);
+        }
+    }
+
     /**
      * Display a listing of the resource.
      */
-    public function index()
-    {
+    public function index() {
+        // menambahkan variable first music cover ke dalam collection playlists
+        $playlists = Playlist::where('user_id', auth()->user()->id)->get();
+        foreach($playlists as $playlist) {
+            // jika merupakan liked music, hard code mengarah ke cover liked music
+            if($playlist->name == 'liked songs') {
+                $playlist->first_music_cover = 'img/liked-music.png';
+                continue;
+            }
+
+            $musics = $playlist->musics;
+            $icon = null;
+
+            // cek isi semua lagu
+            foreach($musics as $music) {
+                // jika ada icon, maka jadikan icon
+                if($music->icon != null) {
+                    $icon = $music->icon;
+                    break;
+                }
+            }
+
+            // simpan ke dalam collection
+            $playlist->first_music_cover = $icon;
+        }
+
         return view('user.library', [
             "title" => "library",
             "active" => "library",
-            "playlists" => Playlist::where('user_id', auth()->user()->id)->get()
+            "playlists" => $playlists
         ]);
     }
 
     /**
      * Show the form for creating a new resource.
      */
-    public function create()
-    {
+    public function create() {
         //
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
-    {
+    public function store(Request $request) {
         $request->validate([
             'playlist_name' => 'required|string|max:255',
             'selected_songs' => 'required|string'
@@ -72,8 +138,7 @@ class PlaylistController extends Controller
     /**
      * Display the specified resource.
      */
-    public function show(Playlist $playlist)
-    {
+    public function show(Playlist $playlist) {
         return [
             'playlist' => $playlist,
             'musics' => $playlist->musics
@@ -83,16 +148,14 @@ class PlaylistController extends Controller
     /**
      * Show the form for editing the specified resource.
      */
-    public function edit(Playlist $playlist)
-    {
+    public function edit(Playlist $playlist) {
         //
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, Playlist $playlist)
-    {
+    public function update(Request $request, Playlist $playlist) {
         // Validate
         $request->validate([
             'playlist_name' => 'required|string|max:255',
@@ -118,8 +181,7 @@ class PlaylistController extends Controller
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(Playlist $playlist)
-    {
+    public function destroy(Playlist $playlist) {
         $playlist->delete();
         return redirect()->back()->with('success', 'Playlist deleted successfully!');
     }
--- resources/views/user/nowPlaying.blade.php
+++ resources/views/user/nowPlaying.blade.php
@@ -1,112 +1,99 @@
 @extends('user.layouts.user_main')
 @section('container')
-{{-- untuk ajax --}}
-<meta name="csrf-token" content="{{ csrf_token() }}">
+    <div class="col scrollable-div p-4" id="jumphere">
+        <div class="container-fluid vh-100 d-flex flex-column">
+            {{-- MUSIC SECTION --}}
+            @if (isset($music))
+                {{-- Playlist Name --}}
+                @if (isset($playlist))
+                    <div class="row d-flex justify-content-center">
+                        <h1 class="fontMonsseratSemiBold text-center text-warning" style="font-size: 30px;">Playlist : <span
+                                class="text-white">{{ $playlist->name }}</span></h1>
+                    </div>
+                @endif
 
-{{-- main --}}
-<div class="col scrollable-div p-4" id="jumphere">
-    <div class="container-fluid vh-100 d-flex flex-column">
-        @if (isset($music))
-        {{-- jika ada --}}
-        <div class="row flex-grow-1">
-            <div class="col d-flex align-items-center justify-content-center m-3">
-                {{-- TODO: tambahkan icon di database music --}}
-                <img src="@if ($music->cover_path) {{ asset('storage/' . $music->cover_path) }} @else {{ asset('img/now_playing/empty_icon.jpeg') }} @endif" alt="Artist Photo" class="img-fluid rounded-3" style="max-width: 500px; max-height: 500px;">
-            </div>
-        </div>
+                {{-- Music Cover --}}
+                <div class="row flex-grow-1">
+                    <div class="col d-flex align-items-center justify-content-center m-3">
+                        <img src="@if ($music->icon) {{ asset('storage/' . $music->icon) }} @else {{ asset('img/now_playing/empty_icon.jpeg') }} @endif"
+                            alt="Artist Photo" class="img-fluid rounded-3" style="max-width: 300px; max-height: 300px;">
+                    </div>
+                </div>
 
-        {{-- title --}}
-        <div class="row d-flex justify-content-center">
-            <h1 class="fontMonsseratSemiBold text-center" style="font-size: 30px;">{{ $music->title }}</h1>
-        </div>
+                {{-- Title --}}
+                <div class="row d-flex justify-content-center">
+                    <h1 class="fontMonsseratSemiBold text-center" style="font-size: 30px;" id="myTitle">
+                        {{ $music->title }}
+                    </h1>
+                </div>
 
-        {{-- artist --}}
-        <div class="row d-flex justify-content-center">
-            <h1 class="fontMontserratThin text-center text-white-50" style="font-size: 12px;">{{ $music->artist }}
-            </h1>
-        </div>
+                {{-- Artist --}}
+                <div class="row d-flex justify-content-center">
+                    <h1 class="fontMontserratThin text-center text-white-50" style="font-size: 12px;" id="myArtist">
+                        {{ $music->artist }}
+                    </h1>
+                </div>
 
-        <div class="row">
-            <div class="col d-flex justify-content-center my-3" style="font-size: 30px;">
-                {{-- TODO: rapikan/hilangkan tampilan play mp3 --}}
-                <audio controls>
-                    <source src="{{ asset('storage/' . $music->file_path) }}" type="audio/mpeg" id="myAudio">
-                </audio>
-                <form action="{{ route( 'user.addlike', ['music' => $music] ) }}" method="post">
-                    @method('post')
+                <div class="row">
+                    <div class="col d-flex justify-content-center my-3" style="font-size: 30px;">
+                        {{-- Bookmark --}}
+                        <a data-id="{{ $music->id }}" data-bs-toggle="modal" data-bs-target="#bookmark-modal" id="bookmark-btn"><i class="bi bi-bookmark-fill mx-3"></i></a>
+
+                        {{-- Audio --}}
+                        <audio controls>
+                            <source src="{{ asset('storage/' . $music->file_path) }}" type="audio/mpeg" id="myAudio">
+                        </audio>
+
+                        {{-- Like --}}
+                        <form action="{{ route('user.addlike', ['music' => $music]) }}" method="post">
+                            @method('post')
+                            @csrf
+                            <button type="submit" name="addlike" id="addlike"
+                                style="background: none; color: inherit; border: none; padding: 0;"><i
+                                    class="bi @if ($isLiked) bi-heart-fill @else bi-heart @endif mx-3"></i></button>
+                        </form>
+                    </div>
+                </div>
+
+                {{-- PLAYLIST TABLE --}}
+                @if (isset($playlist))
                     @csrf
-                    <button type="submit" class="btn btn-dark" name="addlike" id="addlike"><i class="bi bi-heart-fill text-white"></i></button>
-                </form>
-            </div>
-        </div>
-        @elseif(isset($playlist->musics))
-        <div class="row d-flex justify-content-center">
-            <h1 class="fontMonsseratSemiBold text-center text-warning" style="font-size: 30px;">Playlist : <span class="text-white">{{ $playlist->name }}</span></h1>
-        </div>
-        {{-- jika ada playlist --}}
-        {{-- jika ada --}}
-        <div class="row flex-grow-1">
-            <div class="col d-flex align-items-center justify-content-center m-3">
-                {{-- TODO: tambahkan icon di database music --}}
-                <img src="@if ($selected->icon) {{ asset('storage/' . $selected->icon) }} @else {{ asset('img/now_playing/empty_icon.jpeg') }} @endif" alt="Artist Photo" class="img-fluid rounded-3" style="max-width: 300px; max-height: 300px;">
-            </div>
-        </div>
+                    <div class="container-fluid text-left mb-5" id="2">
+                        <table class="table table-striped table-hover table-dark fontMonsseratRegular">
+                            <thead>
+                                <tr>
+                                    <th scope="col">Song</th>
+                                    <th scope="col">Artist</th>
+                                    <th scope="col"></th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                @foreach ($musics as $item)
+                                    <tr>
+                                        <th>{{ $item->title }}</th>
+                                        <th>{{ $item->artist }}</th>
+                                        <th><a
+                                                href="{{ route('user.nowPlaying') . '/?playlist_id=' . request('playlist_id') . '&index=' . $loop->index }}"><i
+                                                    class="bi bi-play-fill text-white"></i></a></th>
+                                    </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
+                    </div>
 
-        {{-- title --}}
-        <div class="row d-flex justify-content-center">
-            <h1 class="fontMonsseratSemiBold text-center" style="font-size: 30px;" id="myTitle">
-                {{ $selected->title }}
-            </h1>
-        </div>
+                    <script></script>
+                @endif
 
-        {{-- artist --}}
-        <div class="row d-flex justify-content-center">
-            <h1 class="fontMontserratThin text-center text-white-50" style="font-size: 12px;" id="myArtist">
-                {{ $selected->artist }}
-            </h1>
+                {{-- WITHOUT MUSIC SECTION --}}
+            @else
+                <div class="container-fluid text-center my-5">
+                    <h1 class="fontMonsseratSemiBold" style="font-size: 20px;">No Music Currently Playing. Play your first
+                        music <a href="{{ route('user.index') }}" class="text-warning"> now!</a></h1>
+                </div>
+            @endif
         </div>
-
-        <div class="row">
-            <div class="col d-flex justify-content-center my-3" style="font-size: 30px;">
-                {{-- TODO: rapikan/hilangkan tampilan play mp3 --}}
-                <audio controls>
-                    <source src="{{ asset('storage/' . $selected->file_path) }}" type="audio/mpeg" id="myAudio">
-                </audio>
-            </div>
-        </div>
-
-
-        {{-- Table Playlist --}}
-        <div class="container-fluid text-left mb-5" id="2">
-            <table class="table table-striped table-hover table-dark fontMonsseratRegular">
-                <thead>
-                    <tr>
-                        <th scope="col">Song</th>
-                        <th scope="col">Artist</th>
-                        <th scope="col"></th>
-                    </tr>
-                </thead>
-                <tbody>
-                    @foreach ($musics as $item)
-                    <tr>
-                        <th>{{ $item->title }}</th>
-                        <th>{{ $item->artist }}</th>
-                        <th><a href="{{ route('user.nowPlaying') . '/?playlist_id=' . request('playlist_id') . '&index=' . $loop->index }}"><i class="bi bi-play-fill text-white"></i></a></th>
-                    </tr>
-                    @endforeach
-                </tbody>
-            </table>
-        </div>
-
-        <script>
-            // hapus karena sistem berubah
-        </script>
-        @else
-        {{-- jika tidak ada music --}}
-        <div class="container-fluid text-center my-5">
-            <h1 class="fontMonsseratSemiBold" style="font-size: 20px;">No Music Currently Playing. Play your first music <a href="{{ route('user.index') }}" class="text-warning"> now!</a></h1>
-        </div>
-        @endif
     </div>
-</div>
+
+    {{-- MODAL --}}
+    @include('user.partials.modalBookmark')
 @endsection
