9148a7bad9b39b681c9f083d72004a7d8c384312
Jovan Andhika <c14220013@john.petra.ac.id>
2023-12-07 09:57:57 +0700

    hapus search

--- resources/views/user/history.blade.php
+++ resources/views/user/history.blade.php
@@ -8,26 +8,9 @@
                     <h1 class="fontMonsseratExtraBold" style="font-size: 50px;">Your History</h1>
                 </div>
 
-                <div class="container-fluid text-center justify-content-center align-content-center flex-nowrap mb-4">
+               
 
-                    <!-- @JovanAndhika @JackGame31 search dari user masuk ke input ini (untuk query) -->
-                    <input type="text" class="form-control fontMonsseratRegular" placeholder="Time is relative..."
-                        aria-label="Username" aria-describedby="addon-wrapping">
-
-                </div>
-
-                <div class="container-fluid d-flex justify-content-center text-center mb-5">
-                    <div class="container-fluid">
-                        <div class="btn-group fontMonsseratExtraBold">
-
-                            <!-- @DanielCJ12479 @JackGame31 @JovanAndhika @royJuanAndika @terrGit ini harus buat listener js kek e. jadi kalau pencet "Search by Title", jadi ne query ne mungkin "SELECT * FROM songs WHERE title = $searched" -->
-
-                            <button type="button" class="btn btn-outline-light ">Search by Song</button>
-                            <button type="button" class="btn btn-outline-light ">Search by Date</button>
-                            <button type="button" class="btn btn-outline-light ">Search by Artist</button>
-                        </div>
-                    </div>
-                </div>
+         
 
 
 
